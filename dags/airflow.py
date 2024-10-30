3d61b0c4487d3626d76da236df363ce2801a281b
RahulNenavath <rahulnenavath205@gmail.com>
2024-10-29 22:18:18 -0400

    column name sanitization | PEP8 formatting | doc string added | parquet file in/out

--- dags/airflow.py
+++ dags/airflow.py
@@ -1,93 +1,107 @@
+import logging
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime, timedelta
 from airflow.utils.trigger_rule import TriggerRule
-from scripts.preprocessing import data_loading, minimum_word_check, detect_language, aggregate_filtered_task, data_cleaning, remove_special_characters, remove_abusive_data
+from scripts.preprocessing import (
+    load_data,
+    filter_records_by_word_count,
+    filter_records_by_language,
+    aggregate_filtered_task,
+    data_cleaning,
+    remove_special_characters,
+    remove_abusive_data,
+)
 
 # Default arguments for the DAG
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 MIN_WORD: int = 5
 
 # Data Validation DAG
 with DAG(
-    'Data_validation_pipeline',
+    "Data_validation_pipeline",
     default_args=default_args,
-    description='DAG for Data Validation',
-    schedule_interval=timedelta(days=1),  
+    description="DAG for Data Validation",
+    schedule_interval=timedelta(days=1),
     start_date=datetime(2024, 10, 17),
     catchup=False,
 ) as dag:
-    
+
     # Task 1: Load the data
     data_loading_task = PythonOperator(
-        task_id = 'data_loading',
-        python_callable=data_loading,
+        task_id="load_data",
+        python_callable=load_data,
     )
     # Task 2: Removing records with less than 5 words
     minimum_words_filter = PythonOperator(
-        task_id = 'minimum_words_filter',
-        python_callable=minimum_word_check,
+        task_id="remove_records_with_minimum_words",
+        python_callable=filter_records_by_word_count,
         op_args=[data_loading_task.output, MIN_WORD],
     )
     # Task 3: Detect language and remove un-recognized language
     language_filter = PythonOperator(
-        task_id = 'detect_language',
-        python_callable=detect_language,
+        task_id="detect_language",
+        python_callable=filter_records_by_language,
         op_args=[minimum_words_filter.output],
     )
     # Task 4: Aggregate results from Task 2 & Task 3
     aggregate_parallel_tasks = PythonOperator(
-        task_id = 'validation_aggregation',
+        task_id="validation_aggregation",
         python_callable=aggregate_filtered_task,
         op_args=[minimum_words_filter.output, language_filter.output],
         provide_context=True,
     )
-    # Task 4: Trigger Data Cleaning DAG
-    TriggerDagRunOperator = TriggerDagRunOperator(
-        task_id='data_cleaning_trigger',
+    # Task 5: Trigger Data Cleaning DAG
+    trigger_data_cleaning_dag_task = TriggerDagRunOperator(
+        task_id="data_cleaning_trigger",
         trigger_rule=TriggerRule.ALL_DONE,
-        trigger_dag_id='Data_cleaning_pipeline',
-        dag=dag
-        )
+        trigger_dag_id="Data_cleaning_pipeline",
+        dag=dag,
+    )
 
 # Data Preprocessing DAG
 with DAG(
-    'Data_cleaning_pipeline',
+    "Data_cleaning_pipeline",
     default_args=default_args,
-    description='DAG for Data Preprocessing',
-    schedule_interval=timedelta(days=1),  
+    description="DAG for Data Preprocessing",
+    schedule_interval=timedelta(days=1),
     start_date=datetime(2024, 10, 17),
     catchup=False,
 ) as dag:
 
     # Task 1: Data Cleaning
     data_cleaning_task = PythonOperator(
-        task_id='datacleaning_process',
+        task_id="datacleaning_process",
         python_callable=data_cleaning,
         provide_context=True,
     )
 
     # Task 2: Remove Special Characters
     remove_special_characters_task = PythonOperator(
-        task_id='remove_special_characters',
+        task_id="remove_special_characters",
         python_callable=remove_special_characters,
-        op_args=[data_cleaning_task.output]
+        op_args=[data_cleaning_task.output],
     )
 
     # Task 3: Remove Abusive Data
     remove_abusive_task = PythonOperator(
-        task_id='remove_abusive_data_task',
+        task_id="remove_abusive_data_task",
         python_callable=remove_abusive_data,
-        op_args=[remove_special_characters_task.output]
+        op_args=[remove_special_characters_task.output],
     )
 
-data_loading_task >> [language_filter, minimum_words_filter] >> aggregate_parallel_tasks >> TriggerDagRunOperator
+(
+    data_loading_task
+    >> [language_filter, minimum_words_filter]
+    >> aggregate_parallel_tasks
+    >> trigger_data_cleaning_dag_task
+)
--- dags/scripts/preprocessing.py
+++ dags/scripts/preprocessing.py
@@ -7,165 +7,256 @@ from lingua import Language, LanguageDetectorBuilder
 from rbloom import Bloom
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-logging.basicConfig(
-    filename='preprocessinglog.txt',
-    level=logging.INFO,
-    filemode='a'
+logging.basicConfig(filename="preprocessinglog.txt", level=logging.INFO, filemode="a")
+
+
+def load_data() -> str:
+    """
+    Load the JPMorgan Chase complaints dataset in Parquet format.
+    Returns:
+        str: Serialized dataset in JSON format.
+    Raises:
+        Exception: If there is an error loading the dataset.
+    """
+    data_path = os.path.join(os.path.dirname(__file__), "../../data/dataset.parquet")
+    try:
+        dataset = pl.read_parquet(data_path).serialize(format="json")
+        return dataset
+    except Exception as error:
+        print(f"Exception:\n{error}")
+        raise Exception("Error With Dataset Loading")
+
+
+def filter_records_by_word_count(dataset: str, min_word_length: int) -> str:
+    """
+    Remove records from the dataset that do not meet the minimum word count
+    in the 'complaint' column.
+    Args:
+        dataset (str): Serialized dataset in JSON format.
+        min_word_length (int): Minimum word count required for each record.
+    Returns:
+        str: Serialized dataset in JSON format with records removed if they
+             have fewer words than the specified minimum.
+    """
+    # Deserialize the dataset
+    dataset = pl.DataFrame.deserialize(io.StringIO(dataset), format="json")
+
+    # Filter records based on the minimum word count and remove the count column
+    dataset = (
+        dataset.with_columns(num_words=pl.col("complaint").str.split(" ").list.len())
+        .filter(pl.col("num_words") > min_word_length)
+        .drop("num_words")
     )
 
-def data_loading() -> str:
-    data_path = os.path.join(os.path.dirname(__file__), "../../data/JPMORGAN_CHASE_COMPLAINTS.csv")
-    try:
-        # Serialize and return the loaded dataset
-        dataset = pl.read_csv(data_path).serialize(format='json')
-        logging.info('dataset loading complete!')
-        return dataset
-    except Exception as e:
-        print(f'Exception:\n{e}')
-        raise("Error With Dataset Loading")
+    # Serialize and return the filtered dataset
+    return dataset.serialize(format="json")
 
-def minimum_word_check(
-        dataset: str,
-        min_word_length: int
-        ) -> str:
-    # Deserialize the dataset and filter out the records that don't match minimum word length
-    dataset = pl.DataFrame.deserialize(io.StringIO(dataset), format='json')
-    # Filtering based on Number of words
-    dataset = dataset.with_columns(
-        num_words = pl.col('Consumer complaint narrative').str.split(' ').list.len()
-        ).filter(
-            pl.col('num_words') > min_word_length
-            )
-    logging.info('Min word check complete!')
-    # Serialize and return dataset
-    return dataset.serialize(format='json')
 
-def detect_language(
-    dataset: str
-    ) -> str:
-    
-    # Deserialize the dataset and filter out the records that don't meet the language criteria
-    dataset = pl.DataFrame.deserialize(io.StringIO(dataset), format='json')
-    
-    # Setup language detector
+def filter_records_by_language(dataset: str) -> str:
+    """
+    Detect the language of each complaint in the dataset and filter out records
+    that do not meet the specified language criteria ('HI' or 'EN').
+    Args:
+        dataset (str): Serialized dataset in JSON format.
+    Returns:
+        str: Serialized dataset in JSON format with records filtered to only
+             include specified languages.
+    """
+    # Deserialize the dataset
+    dataset = pl.DataFrame.deserialize(io.StringIO(dataset), format="json")
+
+    # Initialize language detector
     languages = list(Language.all_spoken_ones())
     detector = LanguageDetectorBuilder.from_languages(*languages).build()
 
-    # Multi-threading the language detection task
+    # Perform language detection with multi-threading
     language_detected = []
     with ThreadPoolExecutor(max_workers=10) as executor:
-        # Submit tasks to the thread pool
-        future_to_sentence = {executor.submit(detector.detect_language_of, sentence): sentence for sentence in dataset['Consumer complaint narrative'].to_list()}
+        future_to_sentence = {
+            executor.submit(detector.detect_language_of, sentence): sentence
+            for sentence in dataset["complaint"].to_list()
+        }
 
-        # Collect the results as they complete
+        # Collect results as they complete
         for future in as_completed(future_to_sentence):
             language_detected.append(future.result())
 
-    language_detected = [item.iso_code_639_1.name for item in language_detected]
-    # Adding and filtering based on language column
-    dataset.with_columns(
-        pl.Series(name='language', values=language_detected)
-    ).filter(
-        pl.col('language').is_in(['HI', 'EN'])
-    ).drop(['language'])
+    # Convert detected languages to ISO codes
+    language_codes = [item.iso_code_639_1.name for item in language_detected]
 
-    logging.info('Language detection complete!')
-    # Serialize and return dataset
-    return dataset.serialize(format='json')
-
-def aggregate_filtered_task(
-        dataset_a: str,
-        dataset_b: str,
-        ) -> None:
-    
-    output_path = os.path.join(os.path.dirname(__file__), "../../data/PREPROCESSED_JPMORGAN_CHASE_COMPLAINTS.parquet")
-
-    # Deserialize Set A and Set B and Join them based on their company id
-    dataset_a = pl.DataFrame.deserialize(io.StringIO(dataset_a), format='json')
-    dataset_b = pl.DataFrame.deserialize(io.StringIO(dataset_b), format='json')
-    # Dont select additional columns
-    dataset_joined = dataset_a.join(dataset_b, on='Complaint ID', how='inner').select(['Date received','Product','Sub-product','Issue','Sub-issue','Consumer complaint narrative','Company public response','Company', 'State','ZIP code','Tags','Consumer consent provided?','Submitted via','Date sent to company','Company response to consumer','Timely response?','Consumer disputed?','Complaint ID','Department',])
-    # Write the output to the output file
-    dataset_joined.write_parquet(output_path, compression='gzip')
-
-def data_cleaning() -> str:
-    # Read the Dataset processed from other DAG
-    datapath = os.path.join(os.path.dirname(__file__), "../../data/PREPROCESSED_JPMORGAN_CHASE_COMPLAINTS.parquet")
-    dataset = pl.read_parquet(datapath)
-
-    # Lowercase complaints
-    dataset = dataset.with_columns(
-        pl.col('Consumer complaint narrative').str.to_lowercase().alias('Consumer complaint narrative')
+    # Add language column to dataset and filter based on language criteria
+    dataset = (
+        dataset.with_columns(pl.Series(name="language", values=language_codes))
+        .filter(pl.col("language").is_in(["HI", "EN"]))
+        .drop(["language"])
     )
 
-    # Deduplicate the records
-    dataset = dataset.unique(subset=['Product', 'Sub-product', 'Consumer complaint narrative'], maintain_order=True)
-    
-    # Drop Nulls
-    dataset = dataset.drop_nulls(subset=['Product', 'Sub-product', 'Department', 'Consumer complaint narrative'])
+    # Serialize and return the filtered dataset
+    return dataset.serialize(format="json")
 
-    # Serialize and return dataset
-    return dataset.serialize(format='json')
+
+def aggregate_filtered_task(dataset_a: str, dataset_b: str) -> None:
+    """
+    Aggregate two datasets by joining them on 'complaint_id' and selecting
+    specific columns. The result is saved to a specified parquet file.
+    Args:
+        dataset_a (str): Serialized first dataset in JSON format.
+        dataset_b (str): Serialized second dataset in JSON format.
+    """
+    output_path = os.path.join(
+        os.path.dirname(__file__),
+        "../../data/preprocessed_dataset.parquet",
+    )
+
+    # Deserialize datasets and perform an inner join on 'Complaint ID'
+    dataset_a = pl.DataFrame.deserialize(io.StringIO(dataset_a), format="json")
+    dataset_b = pl.DataFrame.deserialize(io.StringIO(dataset_b), format="json")
+
+    # Join datasets and select specified columns
+    selected_columns = [
+        "complaint_id",
+        "date_received",
+        "date_resolved",
+        "time_resolved_in_days",
+        "complaint",
+        "complaint_hindi",
+        "product",
+        "department",
+        "sub_product",
+        "issue",
+        "sub_issue",
+        "company",
+        "state",
+        "zipcode",
+        "tags",
+        "company_response_public",
+        "company_response_consumer",
+        "consumer_consent_provided",
+        "submitted_via",
+        "date_sent_to_company",
+        "timely_response",
+        "consumer_disputed",
+    ]
+    dataset_joined = dataset_a.join(dataset_b, on="complaint_id", how="inner").select(
+        selected_columns
+    )
+
+    # Write the output to the specified parquet file
+    dataset_joined.write_parquet(output_path)
+
+
+def data_cleaning() -> str:
+    """
+    Clean the dataset by lowercasing complaint narratives, removing duplicates,
+    and dropping records with null values in key columns.
+    Returns:
+        str: Serialized cleaned dataset in JSON format.
+    """
+    # Define the data path and read the dataset
+    data_path = os.path.join(
+        os.path.dirname(__file__),
+        "../../data/preprocessed_dataset.parquet",
+    )
+    dataset = pl.read_parquet(data_path)
+
+    # Lowercase complaint narratives
+    dataset = dataset.with_columns(pl.col("complaint").str.to_lowercase())
+
+    # Remove duplicate records based on specific columns
+    dataset = dataset.unique(
+        subset=["product", "sub_product", "complaint"],
+        maintain_order=True,
+    )
+
+    # Drop records with nulls in specified columns
+    dataset = dataset.drop_nulls(
+        subset=["product", "sub_product", "department", "complaint"]
+    )
+
+    # Serialize and return the cleaned dataset
+    return dataset.serialize(format="json")
 
 
 def remove_special_characters(dataset: str) -> str:
-    # Deserialize dataset
-    dataset = pl.DataFrame.deserialize(io.StringIO(dataset), format='json')
-    # Removing Special Characters
+    """
+    Remove special characters from the 'complaint' column
+    in the dataset.
+    Args:
+        dataset (str): Serialized dataset in JSON format.
+    Returns:
+        str: Serialized dataset in JSON format with special characters removed
+             from 'complaint'.
+    """
+    # Deserialize the dataset
+    dataset = pl.DataFrame.deserialize(io.StringIO(dataset), format="json")
+
+    # Remove special characters from 'complaint' column
     dataset = dataset.with_columns(
-        pl.col('Consumer complaint narrative').map_elements(lambda x: re.sub(r'[^A-Za-z0-9\s]', '', x), return_dtype=pl.Utf8).alias('Consumer complaint narrative')
+        pl.col("complaint").map_elements(
+            lambda x: re.sub(r"[^A-Za-z0-9\s]", "", x), return_dtype=pl.Utf8
+        )
     )
-    # Serialize and return dataset
-    return dataset.serialize(format='json')
+
+    # Serialize and return the modified dataset
+    return dataset.serialize(format="json")
 
 
-def remove_abusive_data(
-        dataset: str,
-        abuse_placeholder: str = 'yyy'
-        ) -> str:
-    # Deserialize dataset
-    dataset = pl.DataFrame.deserialize(io.StringIO(dataset), format='json')
-    # Set Output Path
-    output_path = os.path.join(os.path.dirname(__file__), "../../data/PREPROCESSED_JPMORGAN_CHASE_COMPLAINTS.parquet")
+def remove_abusive_data(dataset: str, abuse_placeholder: str = "yyy") -> str:
+    """
+    Remove abusive words from 'complaint' column in the dataset,
+    replacing them with a specified placeholder. The cleaned dataset is saved to a
+    predefined output path.
+    Args:
+        dataset (str): Serialized dataset in JSON format.
+        abuse_placeholder (str): Placeholder to replace abusive words.
+    Returns:
+        str: Serialized dataset with abusive words removed.
+    """
+    # Define paths for input and output
+    output_path = os.path.join(
+        os.path.dirname(__file__),
+        "../../data/preprocessed_dataset.parquet",
+    )
+    abusive_words_path = os.path.join(
+        os.path.dirname(__file__), "../../data/profanity_bank_dataset.parquet"
+    )
 
-    # Setup Bloom Filter
+    # Deserialize the dataset
+    dataset = pl.DataFrame.deserialize(io.StringIO(dataset), format="json")
+
+    # Set up Bloom Filter for abusive words
     profane_set = set()
     profanity_bloom = Bloom(200_000, 0.01)
 
     # Load abusive words
-    abusive_words_path = os.path.join(os.path.dirname(__file__), "../../data/profanity_bank_dataset.csv")
-    abusive_words = pl.read_csv(abusive_words_path).with_columns(
-        pl.col('Profanity').str.to_lowercase()
-        ).filter(
-            pl.col('Profanity').is_not_null()
-            )['Profanity'].to_list()
+    abusive_words = (
+        pl.read_parquet(abusive_words_path)["profanity"].to_list()
+    )
 
-    for item in abusive_words:
-        profanity_bloom.add(item)
-        profane_set.add(item)
-    
-    # Tokenize Complaints
-    tokenized_complaints = dataset.with_columns(
-        pl.col('Consumer complaint narrative').str.split(' ')
-    )['Consumer complaint narrative'].to_list()
+    for word in abusive_words:
+        profanity_bloom.add(word)
+        profane_set.add(word)
 
-    # Remove abusive words
+    # Tokenize and clean complaints
+    tokenized_complaints = dataset.with_columns(pl.col("complaint").str.split(" "))[
+        "complaint"
+    ].to_list()
+
     cleaned_records = []
-    for idx, record in enumerate(tokenized_complaints):
-        clean_record = []
-        for w in record:
-            if w not in profanity_bloom:
-                clean_record.append(w)
-            # The 10% error case or the token is actually a profane word
-            elif w in profane_set:
-                clean_record.append(abuse_placeholder)
-            else:
-                clean_record.append(w)
-        
+    for record in tokenized_complaints:
+        clean_record = [
+            w if w not in profanity_bloom or w not in profane_set else abuse_placeholder
+            for w in record
+        ]
         cleaned_records.append(" ".join(clean_record))
 
-    dataset.with_columns(
-        pl.Series(name='abuse_free_complaints', values=cleaned_records)
+    # Add the cleaned complaints to the dataset
+    dataset = dataset.with_columns(
+        pl.Series(name="abuse_free_complaints", values=cleaned_records)
     )
-    # Save the processed results to output path
-    dataset.write_parquet(output_path, compression='gzip')
+
+    # Save the processed dataset to output path
+    dataset.write_parquet(output_path)
+
+    # Return the serialized dataset
+    return dataset.serialize(format="json")
