69c4bd7e474fb222482368b19cbf5bc3e17e7d56
RahulNenavath <rahulnenavath205@gmail.com>
2024-11-20 00:59:11 -0500

    added inference pipeline validation code

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -1,9 +1,35 @@
 import os
+from typing import Dict, List, Any
+from fast_langdetect import detect_language
 
-class PreprocessingPieline:
+
+class DataValidationPipeline:
+    def __init__(self, checks: Dict[str, str]):
+        self.min_words: int = checks.get('min_words')
+        self.max_words: int = checks.get('max_words')
+        self.allowed_languages: List[str] = checks.get("allowed_languages")
+
+    def _accepted_word_count_check(self, num_words: int) -> bool:
+        return self.min_words < num_words < self.max_words
+    
+    def _language_check(self, text: str) -> bool:
+        text_language = detect_language(text)
+        return text_language in self.allowed_languages
+
+    def is_valid(self, text: str) -> bool:
+        text = text.lower()
+        words = text.split(' ')
+        num_words_check = self._accepted_word_count_check(len(words))
+        language_check = self._language_check(text)
+        return num_words_check and language_check
+
+class DataTransformationPipeline:
     def __init__(self, min_word_length: int = 5):
         pass
     def process(self, text: str):
+        words  = text.split('')
+        if len(words) < min_word_length:
+            return None
         return text.lower()
 
 
--- backend/server.py
+++ backend/server.py
@@ -1,26 +1,45 @@
 import os
-import re
 import uvicorn
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import Optional
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+from object_models import Complaint, PredictionResponse, ErrorResponse
+from preprocessing import DataValidationPipeline, DataTransformationPipeline
 
-from preprocessing import PreprocessingPieline
-
-class Complaint(BaseModel):
-    complaint_text: str
-
-class PredictionResponse(BaseModel):
-    product: str
-    department: str
-    processed_text: str
-
-preprocessing_pipeline = PreprocessingPieline()
+validation_checks = {
+    'min_words': 5,
+    'max_words': 300
+}
+validation_pipeline = DataValidationPipeline(validation_checks)
+preprocessing_pipeline = DataTransformationPipeline()
 
 app = FastAPI(
     title="MLOps - Bilingual Complaint Classification System",
     description="Backend API Server to handle complaints from banking domain",
     version="0.1",
 )
+
+class CustomException(HTTPException):
+    def __init__(self, 
+                 status_code: int,
+                 error_code: int, error_message: str, 
+                 details: Optional[str] = None):
+        self.error_code = error_code
+        self.error_message = error_message
+        self.details = details
+        super().__init__(status_code=status_code, detail=error_message)
+
+@app.exception_handler(CustomException)
+async def custom_exception_handler(request: Request, exc: CustomException):
+    error_response = ErrorResponse(
+        error_code=exc.error_code,
+        error_message=exc.error_message,
+        details=exc.details
+    )
+    return JSONResponse(
+        status_code=exc.status_code, 
+        content=error_response.dict()
+        )
 
 @app.get("/ping")
 def ping():
@@ -31,9 +50,21 @@ def ping():
 
 @app.post("/predict")
 async def submit_complaint(complaint: Complaint, response_model=PredictionResponse):
+
+    try:
+        validation_pipeline.is_valid(text=complaint.complaint_text)
+    except Exception as e:
+        raise CustomException(
+            status_code=400,
+            error_code=1001,
+            error_message="Prediction failed due to invalid input",
+            details=str(e)
+        )
+    
     processed_text = preprocessing_pipeline.process(text=complaint.complaint_text)
     predicted_product = "student loan" 
     predicted_departmet = "loan services"
+
     return PredictionResponse(
         product=predicted_product, 
         department=predicted_departmet,
