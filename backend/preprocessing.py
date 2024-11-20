13201dfd4d2af05546efb7e45003d75ae719b01f
RahulNenavath <rahulnenavath205@gmail.com>
2024-11-19 23:40:18 -0500

    updated preproc fun

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -4,6 +4,7 @@ class PreprocessingPieline:
     def __init__(self, min_word_length: int = 5):
         pass
     def process(self, text: str):
+        print(f'Hello')
         return text.lower()
 
 
